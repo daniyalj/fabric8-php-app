571c80fd749260c9f011a40fd71dd5103b62711f
daniyalj <daniyaljavedk@gmail.com>
2017-04-04 12:28:10 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <body>
+  <img src="https://cloud.githubusercontent.com/assets/13129393/23463437/7cd45ea0-fe5f-11e6-9618-684be5b9318e.png" alt="ms">
+
+  <title>fabric8 example</title>
+  <?php
+  echo "Hello to CIBC";
+  ?>
+
+  </body>
+</html>
